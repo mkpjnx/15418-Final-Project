4f10c37c0bb088b9299ec08f4428ae6357c820f2
JackMenandCameron <jmcamero@andrew.cmu.edu>
2020-04-29 01:50:14 -0400

    Added sequential and Global com to Intrument.py

--- Instrument.py
+++ Instrument.py
@@ -1,42 +1,54 @@
 import csv
 import subprocess
 import random
+
+mpi = False
+topText = 0
+args = 4
+topText = ("graph size, steps, divides, processes, " +
+    "time startup, per startup, " +
+    "time jacobi, per jacobi, ")
+
+if mpi:
+  topText += ("time local com, per local com, " +
+    "time global com, per global com, ")
+  args += 2
+
+topText += ("time unknown, per unknown, " +
+  "time elapsed, per elapsed\n")
+
 f2 = open("Log/run.csv", "w")
-f2.write("run, graph size, steps, divides, processes, ")
-f2.write("time startup, per startup, ")
-f2.write("time jacobi, per jacobi, ")
-f2.write("time local com, per local com, ")
-f2.write("time unknown, per unknown, ")
-f2.write("time elapsed, per elapsed\n")
+f2.write(topText)
 
 nps = [1,2,3,4,5,6,7,8]
 runs = 5
 for t in range(8):
   print("range ", t)
-  g = 500
-  s = 1000
+  g = 500 * t
+  s = 100
   d = 1
-  np =  nps[t]
-  times = [0,0,0,0,0]
-  percents = [0,0,0,0,0]
+  np = 1# nps[t]
+  times = [0,0,0,0,0,0]
+  percents = [0,0,0,0,0,0]
   for i in range(runs):
+    command = ["./grayscott-seq" , "-g", str(g), "-r", "1", "-s", str(s), "-d", str(d), "-I"]
+    if mpi:
+      command = ["mpirun", "-np", str(np), "./grayscott-mpi", "-g", str(g), "-r", "1", "-s", str(s), "-d", str(d), "-I"]
     print("iteration", i)
 
-
     ou = open("Log/log1.txt", "wb")
-    subprocess.run(["mpirun", "-np", str(np), "./grayscott-mpi", "-g", str(g), "-r", "1", "-s", str(s), "-d", str(d), "-I"],
-                  stdout=ou)
+    subprocess.run(command, stdout=ou)
 
     f1 = open("Log/log1.txt", "r")
-    for a in range(5):
+    for a in range(args):
       line = f1.readline()
       spl = line.split()
       times[a] += int(spl[0])
       percents[a] += float(spl[2])
 
-  description = str(i) + ", " + str(g) + ", " + str(s) + ", " + str(d) + ", " + str(np)
+  description = str(g) + ", " + str(s) + ", " + str(d) + ", " + str(np)
   f2.write(description)
-  for a in range(5):
+  for a in range(args):
     time = times[a]/runs
     percent = percents[a]/runs
     st = ", " + str(time) + ", " + str(percent)
